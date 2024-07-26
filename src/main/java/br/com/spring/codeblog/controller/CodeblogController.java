075f0eab25640272892e9ea873f76051eeff9ca4
Henriquemendes1 <henrique.mend258@gmail.com>
2024-07-26 11:11:59 -0300

    Commit

--- src/main/java/br/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/br/com/spring/codeblog/controller/CodeblogController.java
@@ -3,13 +3,15 @@ package br.com.spring.codeblog.controller;
 
 import br.com.spring.codeblog.model.Post;
 import br.com.spring.codeblog.service.CodeblogService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -26,12 +28,27 @@ public class CodeblogController {
         return mv;
     }
 
-    @RequestMapping(value= "/posts/{id}", method = RequestMethod.GET)
+    @GetMapping("/posts/{id}")
     public ModelAndView getPostDetails(@PathVariable("id") long id){
         ModelAndView mv = new ModelAndView("postDetails");
         Post posts = codeblogService.findById(id);
         mv.addObject("post", posts);
         return mv;
+    }
+
+    @GetMapping("/newpost")
+    public String getPostForm(){
+        return "postForm";
+    }
+
+    @PostMapping("/newpost")
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        codeblogService.save(post);
+        return "redirect:/posts";
     }
 
 
