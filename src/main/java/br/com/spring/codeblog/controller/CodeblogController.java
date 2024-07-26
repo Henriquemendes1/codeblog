0959278599b9069cdb7bd3decce03fcb06ab61c4
Henriquemendes1 <henrique.mend258@gmail.com>
2024-07-26 10:59:20 -0300

    Commit

--- src/main/java/br/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/br/com/spring/codeblog/controller/CodeblogController.java
@@ -5,6 +5,7 @@ import br.com.spring.codeblog.model.Post;
 import br.com.spring.codeblog.service.CodeblogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,6 +25,16 @@ public class CodeblogController {
         mv.addObject("posts", posts);
         return mv;
     }
+
+    @RequestMapping(value= "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id){
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post posts = codeblogService.findById(id);
+        mv.addObject("post", posts);
+        return mv;
+    }
+
+
 
 
 }
